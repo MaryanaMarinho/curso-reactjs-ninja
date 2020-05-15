2ade70d64ade0f842dfdb75a67d20c3b2b68262f
MaryanaMarinho <maryanalirius@hotmail.com>
2020-05-14 22:32:02 -0300

    MRW#04 - Modularizando a aplicação

---
+++ modulo-01/react-webpack/src/app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function sum(val1, val2) {
+    return val1 + val2;
+}
+
+// exportando a funçao para ser utilizada la no index.js
+module.exports = sum
--- modulo-01/react-webpack/src/index.js
+++ modulo-01/react-webpack/src/index.js
@@ -1,1 +1,6 @@
-console.log('webpack esta funcionando!!');
+'use strict'
+
+// importando a funçao que foi exportada no app.js
+var sum = require('./app')
+
+console.log(sum(1, 2));
